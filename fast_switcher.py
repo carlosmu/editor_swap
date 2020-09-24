52737f1236c4b68bf9692019f1420b24189ffc63
carlosmu <carlos.damian.munoz@gmail.com>
2020-09-24 14:07:04 -0300

    Add support for TextEditor / Console

--- fast_switcher.py
+++ fast_switcher.py
@@ -11,11 +11,13 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import bpy
+
 bl_info = {
     "name" : "fast_switcher",
     "author" : "carlosmu <carlos.damian.munoz@gmail.com>",    
     "blender" : (2, 83, 0),
-    "version" : (0, 2, 1),
+    "version" : (0, 3, 0),
     "category" : "User",
     "location" : "Some editors headers",
     "description" : "Fast switching between some editors.",
@@ -24,8 +26,6 @@ bl_info = {
     "tracker_url" : "",
 }
 
-import bpy
-
 # Operator class
 class FS_OT_fast_switcher(bpy.types.Operator):
     """Fast switch between outliner and properties editors"""
@@ -33,6 +33,7 @@ class FS_OT_fast_switcher(bpy.types.Operator):
     bl_label = "Fast Switcher"
     
     # It prevents the operator from appearing in other editors.
+    # To-Do: When we have more editors supported it will be convenient to use an "if not ..." approach.
     @classmethod
     def poll(cls, context):
         if context.area.ui_type == 'OUTLINER':
@@ -50,6 +51,10 @@ class FS_OT_fast_switcher(bpy.types.Operator):
         elif context.area.ui_type == 'UV':
             return True
         elif context.area.ui_type == 'IMAGE_EDITOR':
+            return True
+        elif context.area.ui_type == 'TEXT_EDITOR':
+            return True
+        elif context.area.ui_type == 'CONSOLE':
             return True
         else:
             return False
@@ -70,17 +75,22 @@ class FS_OT_fast_switcher(bpy.types.Operator):
             bpy.context.area.ui_type = 'IMAGE_EDITOR'
         elif (bpy.context.area.ui_type == 'IMAGE_EDITOR'):
             bpy.context.area.ui_type = 'UV'
+        elif (bpy.context.area.ui_type == 'TEXT_EDITOR'):
+            bpy.context.area.ui_type = 'CONSOLE'
+        elif (bpy.context.area.ui_type == 'CONSOLE'):
+            bpy.context.area.ui_type = 'TEXT_EDITOR'
         else: 
             bpy.context.area.ui_type = 'DOPESHEET'
         return{'FINISHED'}
 
 # Draw buttons
 def draw_fast_switcher(self, context):
-    # If not context editor "Drivers" draw buttons (this is children of FCurves). 
+    # If not context editor "Drivers" draw buttons (because this is children of FCurves). 
     if not (bpy.context.area.ui_type == 'DRIVERS'): 
         self.layout.operator("area.fast_switcher",text="", icon='WINDOW')
 
 # Register/unregister the operator class and draw function
+# To-Do: Give more elegance to this code.
 def register():
     bpy.utils.register_class(FS_OT_fast_switcher)
     bpy.types.OUTLINER_HT_header.prepend(draw_fast_switcher)
@@ -89,8 +99,8 @@ def register():
     bpy.types.GRAPH_HT_header.prepend(draw_fast_switcher)    
     bpy.types.INFO_HT_header.prepend(draw_fast_switcher)    
     bpy.types.IMAGE_HT_header.prepend(draw_fast_switcher)    
-    # bpy.types.TEXT_HT_header.prepend(draw_fast_switcher)    
-    # bpy.types.CONSOLE_HT_header.prepend(draw_fast_switcher)    
+    bpy.types.TEXT_HT_header.prepend(draw_fast_switcher)    
+    bpy.types.CONSOLE_HT_header.prepend(draw_fast_switcher)    
         
 def unregister():
     bpy.utils.unregister_class(FS_OT_fast_switcher)
@@ -100,5 +110,5 @@ def unregister():
     bpy.types.GRAPH_HT_header.remove(draw_fast_switcher)
     bpy.types.INFO_HT_header.remove(draw_fast_switcher)
     bpy.types.IMAGE_HT_header.remove(draw_fast_switcher)
-    # bpy.types.TEXT_HT_header.remove(draw_fast_switcher)
-    # bpy.types.CONSOLE_HT_header.remove(draw_fast_switcher)
+    bpy.types.TEXT_HT_header.remove(draw_fast_switcher)
+    bpy.types.CONSOLE_HT_header.remove(draw_fast_switcher)
