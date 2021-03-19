9f0055451166f7c64f8c688f016f04c5261a2546
carlosmu <carlos.damian.munoz@gmail.com>
2021-03-19 01:20:30 -0300

    New User Prefs semi-functional

--- editor_swap/ot_editor_swap.py
+++ editor_swap/ot_editor_swap.py
@@ -20,50 +20,90 @@ class ES_OT_editor_swap(bpy.types.Operator):
 
     # If the current editor is "X" assign "Y"...
     def execute(self, context):
-        if context.area.ui_type == 'OUTLINER':
-            context.area.ui_type = 'PROPERTIES'
-        elif context.area.ui_type == 'PROPERTIES':
-            context.area.ui_type = 'OUTLINER'
-        elif context.area.ui_type == 'DOPESHEET':
-            context.area.ui_type = 'FCURVES'
-        elif context.area.ui_type == 'TIMELINE':
-            context.area.ui_type = 'INFO'
-        elif context.area.ui_type == 'INFO':
-            context.area.ui_type = 'TIMELINE'
-        # Fixing inconsistent Image_Editor names between versions
-        elif context.area.ui_type == 'UV':
-            if bpy.app.version >= (2, 91, 0):
-                context.area.ui_type = 'IMAGE_EDITOR'
-            else: 
-                context.area.ui_type = 'VIEW'
-                # End of fix
-        elif context.area.ui_type == 'VIEW':
-            context.area.ui_type = 'UV'
+        es_view_3d = context.preferences.addons[__package__].preferences.es_view_3d
+        es_image_editor = context.preferences.addons[__package__].preferences.es_image_editor
+        es_uv = context.preferences.addons[__package__].preferences.es_uv
+        es_compositor = context.preferences.addons[__package__].preferences.es_compositor
+        es_texture_node = context.preferences.addons[__package__].preferences.es_texture_node
+        es_geometry_node = context.preferences.addons[__package__].preferences.es_geometry_node
+        es_shader_editor = context.preferences.addons[__package__].preferences.es_shader_editor
+        es_sequence_editor = context.preferences.addons[__package__].preferences.es_sequence_editor
+        es_clip_editor = context.preferences.addons[__package__].preferences.es_clip_editor
+        es_dopesheet = context.preferences.addons[__package__].preferences.es_dopesheet
+        es_timeline = context.preferences.addons[__package__].preferences.es_timeline
+        es_fcurves = context.preferences.addons[__package__].preferences.es_fcurves
+        es_drivers = context.preferences.addons[__package__].preferences.es_drivers
+        es_nla_editor = context.preferences.addons[__package__].preferences.es_nla_editor
+        es_text_editor = context.preferences.addons[__package__].preferences.es_text_editor
+        es_console = context.preferences.addons[__package__].preferences.es_console
+        es_info = context.preferences.addons[__package__].preferences.es_info
+        es_outliner = context.preferences.addons[__package__].preferences.es_outliner
+        es_properties = context.preferences.addons[__package__].preferences.es_properties
+        es_files = context.preferences.addons[__package__].preferences.es_files
+        es_assets = context.preferences.addons[__package__].preferences.es_assets
+        es_preferences = context.preferences.addons[__package__].preferences.es_preferences
+        
+
+
+
+
+        if context.area.ui_type == 'VIEW_3D':
+            context.area.ui_type = es_view_3d
         elif context.area.ui_type == 'IMAGE_EDITOR':
-            context.area.ui_type = 'UV'
-        elif context.area.ui_type == 'TEXT_EDITOR':
-            context.area.ui_type = 'CONSOLE'
-        elif context.area.ui_type == 'CONSOLE':
-            context.area.ui_type = 'TEXT_EDITOR'
+            context.area.ui_type = es_image_editor
+        elif context.area.ui_type == 'UV':
+            context.area.ui_type = es_uv
         elif context.area.ui_type == 'CompositorNodeTree':
-            context.area.ui_type = 'ShaderNodeTree'
+            context.area.ui_type = es_compositor
+        elif context.area.ui_type == 'TextureNodeTree':
+            context.area.ui_type = es_texture_node
+        elif context.area.ui_type == 'GeometryNodeTree':
+            context.area.ui_type = es_geometry_node
         elif context.area.ui_type == 'ShaderNodeTree':
-            context.area.ui_type = 'CompositorNodeTree'
-        elif context.area.ui_type == 'DRIVERS':
-            context.area.ui_type = 'NLA_EDITOR'
-        elif context.area.ui_type == 'NLA_EDITOR':
-            context.area.ui_type = 'DRIVERS'
+            context.area.ui_type = es_shader_editor
         elif context.area.ui_type == 'SEQUENCE_EDITOR':
-            context.area.ui_type = 'CLIP_EDITOR'
+            context.area.ui_type = es_sequence_editor
         elif context.area.ui_type == 'CLIP_EDITOR':
-            context.area.ui_type = 'SEQUENCE_EDITOR'
+            context.area.ui_type = es_clip_editor
+        elif context.area.ui_type == 'DOPESHEET':
+            context.area.ui_type = es_dopesheet
+        elif context.area.ui_type == 'TIMELINE':
+            context.area.ui_type = es_timeline
+        elif context.area.ui_type == 'FCURVES':
+            context.area.ui_type = es_fcurves
+        elif context.area.ui_type == 'DRIVERS':
+            context.area.ui_type = es_drivers
+        elif context.area.ui_type == 'NLA_EDITOR':
+            context.area.ui_type = es_nla_editor
+        elif context.area.ui_type == 'TEXT_EDITOR':
+            context.area.ui_type = es_text_editor
+        elif context.area.ui_type == 'CONSOLE':
+            context.area.ui_type = es_console
+        elif context.area.ui_type == 'INFO':
+            context.area.ui_type = es_info
+        elif context.area.ui_type == 'OUTLINER':
+            context.area.ui_type = es_outliner
+        elif context.area.ui_type == 'PROPERTIES':
+            context.area.ui_type = es_properties
+        # elif context.area.ui_type == 'VIEW':
+        #     context.area.ui_type = es_view
+        elif context.area.ui_type == 'FILES':
+            context.area.ui_type =  es_files
+        elif context.area.ui_type == 'ASSETS':
+            context.area.ui_type = es_assets
         elif context.area.ui_type == 'PREFERENCES':
-            context.area.ui_type = 'VIEW_3D'
-        elif context.area.ui_type == 'VIEW_3D':
-            context.area.ui_type = bpy.context.preferences.addons[__package__].preferences.view_3d,
-        else: 
-            context.area.ui_type = 'DOPESHEET'
+            context.area.ui_type = es_preferences
+        # else: 
+        #     context.area.ui_type = 'DOPESHEET' 
         return{'FINISHED'}
+
+
+        # Fixing inconsistent Image_Editor names between versions
+            # if bpy.app.version >= (2, 91, 0):
+            #     context.area.ui_type = 'IMAGE_EDITOR'
+            # else: 
+            #     context.area.ui_type = 'VIEW'
+                # End of fix
 
 ####################################
 # REGISTER/UNREGISTER
--- editor_swap/user_prefs.py
+++ editor_swap/user_prefs.py
@@ -7,52 +7,169 @@ import bpy
 class ES_UserPrefs(bpy.types.AddonPreferences):
     bl_idname = __package__
 
-    # popup_dialog : bpy.props.BoolProperty(
-    #     name="Enable Popup Dialog on Creation",
-    #     description="Enable or Disable the Popup Dialog on creation of Quick Lattice", 
-    #     default=True
-    #     )
+    popup_dialog : bpy.props.BoolProperty(
+        name="Enable Popup Dialog on Creation",
+        description="Enable or Disable the Popup Dialog on creation of Quick Lattice", 
+        default=True
+        )
 
-    items = [
-        ('NONE', 'None', 'None', '', 0, '', ''),
-        ('VIEW_3D', '3D Viewport', '3D Viewport', 'VIEW3D', 1, 'view_3d', 'VIEW3D_HT_tool_header'),
-        ('VIEW', 'Image Editor', 'Image Editor', 'IMAGE', 2, 'image_editor', 'IMAGE_HT_header') if bpy.app.version < (2, 91, 0)
-        else ('IMAGE_EDITOR', 'Image Editor', 'Image Editor', 'IMAGE', 2, 'image_editor', 'IMAGE_HT_header'),
-        ('UV', 'UV Editor', 'UV Editor', 'UV', 3, 'uv_editor', 'IMAGE_HT_header'),
-        ('ShaderNodeTree', 'Shader Editor', 'Shader Editor', 'SHADING_RENDERED', 4, 'shader_editor', 'NODE_HT_header'),
-        ('CompositorNodeTree', 'Compositor', 'Compositor', 'NODE_COMPOSITING', 5, 'compositor', 'NODE_HT_header'),
-        ('TextureNodeTree', 'Texture Node Editor', 'Texture Node Editor', 'NODE_TEXTURE', 6, 'texture_node_editor', 'NODE_HT_header'),
-        ('SEQUENCE_EDITOR', 'Video Squiencer', 'Video Sequencer', 'SEQUENCE', 7, 'sequence_editor', 'SEQUENCER_HT_header'),
-        ('CLIP_EDITOR', 'Movie Clip Editor', 'Movie Clip Editor', 'TRACKER', 8, 'clip_editor', 'CLIP_HT_header'),
-        ('DOPESHEET', 'Dope Sheet', 'Dope Sheet', 'ACTION', 9, 'doppesheet', 'DOPESHEET_HT_header'),
-        ('TIMELINE', 'TimeLine', 'TimeLine', 'TIME', 10, 'timeline', 'DOPESHEET_HT_header'),
-        ('FCURVES', 'Graph Editor', 'Graph Editor', 'GRAPH', 11, 'fcurves', 'GRAPH_HT_header'),
-        ('DRIVERS', 'Drivers', 'Drivers', 'DRIVER', 12, 'drivers', 'GRAPH_HT_header'),
-        ('NLA_EDITOR', 'Nonlinear Animation', 'Nonlinear Animation', 'NLA', 13, 'nla_editor', 'NLA_HT_header'),
-        ('TEXT_EDITOR', 'Text Editor', 'Text Editor', 'TEXT', 14, 'text_editor', 'TEXT_HT_header'),
-        ('CONSOLE', 'Python Console', 'Python Console', 'CONSOLE', 15, 'console', 'CONSOLE_HT_header'),
-        ('INFO', 'Info', 'Info', 'INFO', 16, 'info', 'INFO_HT_header'),
-        ('OUTLINER', 'Outliner', 'Outliner', 'OUTLINER', 17, 'outliner', 'OUTLINER_HT_header'),
-        ('PROPERTIES', 'Properties', 'Properties', 'PROPERTIES', 18, 'properties', 'PROPERTIES_HT_header'),
-        ('FILE_BROWSER', 'File Browser', 'File Browser', 'FILEBROWSER', 19, 'file_browser', 'FILEBROWSER_HT_header'),
-        ('PREFERENCES', 'Preferences', 'Preferences', 'PREFERENCES', 20, 'preferences', 'USERPREF_HT_header'),
-        ('an_AnimationNodeTree', 'Animation Nodes', 'Animation Nodes', 'ONIONSKIN_ON', 21, 'animation_nodes', 'NODE_HT_header')
-    ]
+    editors = (('VIEW_3D', '3D Viewport', ''),
+            ('IMAGE_EDITOR', 'Image Editor', '')
+            if bpy.app.version >= (2, 91, 0) else
+            ('VIEW', 'Old Image Editor', ''),
+            ('UV', 'UV Editor', ''),
+            ('CompositorNodeTree', 'Compositor', ''),
+            ('TextureNodeTree', 'Texture Node Editor', ''),
+            ('GeometryNodeTree', 'Geometry Node Editor', ''),
+            ('ShaderNodeTree', 'Shader Editor', ''),
+            ('SEQUENCE_EDITOR', 'Video Sequencer', ''),
+            ('CLIP_EDITOR', 'Movie Clip Editor', ''),
+            ('DOPESHEET', 'Dope Sheet', ''),
+            ('TIMELINE', 'Timeline', ''),
+            ('FCURVES', 'Graph Editor', ''),
+            ('DRIVERS', 'Drivers', ''),
+            ('NLA_EDITOR', 'Nonlinear Animation', ''),
+            ('TEXT_EDITOR', 'Text Editor', ''),
+            ('CONSOLE', 'Python Console', ''),
+            ('INFO', 'Info', ''),
+            ('OUTLINER', 'Outliner', ''),
+            ('PROPERTIES', 'Properties', ''),
+            ('FILES', 'File Browser', ''),
+            ('ASSETS', 'Asset Browser', ''),
+            ('PREFERENCES', 'Preferences', '')
+    )
 
-    # view_3d: EnumProperty(
-    #     name='View 3D',
-    #     items=[elem[:5] for elem in items],
-    #     default='ShaderNodeTree',
-    #     update=lambda self, context: AREA_SWITCHER_ui.update(context=context)
-    # )
-    # [(id, name, description, icon, id_num, variable name, area header name), ...]
-    view_3d : bpy.props.EnumProperty(
-        name = "View 3D",
-        description = "Select an editor to pair them",
-        items = [elem[:5] for elem in items],
-        # items = ['EMPTY', 'VIEW_3D', 'IMAGE_EDITOR', 'NODE_EDITOR', 'SEQUENCE_EDITOR', 'CLIP_EDITOR', 'DOPESHEET_EDITOR', 'GRAPH_EDITOR', 'NLA_EDITOR', 'TEXT_EDITOR', 'CONSOLE', 'INFO', 'TOPBAR', 'STATUSBAR', 'OUTLINER', 'PROPERTIES', 'FILE_BROWSER', 'PREFERENCES'],
+    es_view_3d : bpy.props.EnumProperty(
+        name = "3D Viewport",
+        description= "",
+        items = editors,
         default='ShaderNodeTree',
-        # update=lambda self, context: AREA_SWITCHER_ui.update(context=context)
+    )
+    es_image_editor : bpy.props.EnumProperty(
+        name = "Image Editor",
+        description= "",
+        items = editors,
+        default='UV',
+    )
+    es_uv : bpy.props.EnumProperty(
+        name = "UV Editor",
+        description= "",
+        items = editors,
+        default='IMAGE_EDITOR',
+    )
+    es_compositor : bpy.props.EnumProperty(
+        name = "Compositor",
+        description= "",
+        items = editors,
+        default='TextureNodeTree',
+    )
+    es_texture_node : bpy.props.EnumProperty(
+        name = "Texture Node Editor",
+        description= "",
+        items = editors,
+        default='CompositorNodeTree',
+    )
+    es_geometry_node : bpy.props.EnumProperty(
+        name = "Geometry Node Editor",
+        description= "",
+        items = editors,
+        default='GeometryNodeTree',
+    )
+    es_shader_editor : bpy.props.EnumProperty(
+        name = "Shader Editor",
+        description= "",
+        items = editors,
+        default='VIEW_3D',
+    )
+    es_sequence_editor : bpy.props.EnumProperty(
+        name = "Video Sequencer",
+        description= "",
+        items = editors,
+        default='CLIP_EDITOR',
+    )
+    es_clip_editor : bpy.props.EnumProperty(
+        name = "Movie Clip Editor",
+        description= "",
+        items = editors,
+        default='SEQUENCE_EDITOR',
+    )
+    es_dopesheet : bpy.props.EnumProperty(
+        name = "Dope Sheet",
+        description= "",
+        items = editors,
+        default='FCURVES',
+    )
+    es_timeline : bpy.props.EnumProperty(
+        name = "Timeline",
+        description= "",
+        items = editors,
+        default='INFO',
+    )
+    es_fcurves : bpy.props.EnumProperty(
+        name = "Graph Editor",
+        description= "",
+        items = editors,
+        default='DOPESHEET',
+    )
+    es_drivers : bpy.props.EnumProperty(
+        name = "Drivers",
+        description= "",
+        items = editors,
+        default='DRIVERS',
+    )
+    es_nla_editor : bpy.props.EnumProperty(
+        name = "Nonlinear Animation",
+        description= "",
+        items = editors,
+        default='NLA_EDITOR',
+    )
+    es_text_editor : bpy.props.EnumProperty(
+        name = "Text Editor",
+        description= "",
+        items = editors,
+        default='CONSOLE',
+    )
+    es_console : bpy.props.EnumProperty(
+        name = "Python Console",
+        description= "",
+        items = editors,
+        default='TEXT_EDITOR',
+    )
+    es_info : bpy.props.EnumProperty(
+        name = "Info",
+        description= "",
+        items = editors,
+        default='TIMELINE',
+    )
+    es_outliner : bpy.props.EnumProperty(
+        name = "Outliner",
+        description= "",
+        items = editors,
+        default='PROPERTIES',
+    )
+    es_properties : bpy.props.EnumProperty(
+        name = "Properties",
+        description= "",
+        items = editors,
+        default='OUTLINER',
+    )
+    es_files : bpy.props.EnumProperty(
+        name = "File Browser",
+        description= "",
+        items = editors,
+        default='ASSETS',
+    )
+    es_assets : bpy.props.EnumProperty(
+        name = "Asset Browser",
+        description= "",
+        items = editors,
+        default='FILES',
+    )
+    es_preferences : bpy.props.EnumProperty(
+        name = "Preferences",
+        description= "",
+        items = editors,
+        default='PREFERENCES',
     )
     
     def draw(self, context):
@@ -61,12 +178,29 @@ class ES_UserPrefs(bpy.types.AddonPreferences):
         layout.use_property_decorate = True
         box = layout.box()
         box.separator()
-        # box.prop(self, "popup_dialog")
+        box.prop(self, "popup_dialog")
         box.separator()
-        box.prop(self, "view_3d", text="3D Viewport")
-        # box.prop(self, "editor_selector", text="Image Editor")
-        # box.prop(self, "editor_selector", text="UV Editor")
-        # box.prop(self, "editor_selector", text="Shader Editor")
+        box.prop(self, "es_view_3d", text="3D Viewport")
+        box.prop(self, "es_uv", text="Image Editor")
+        box.prop(self, "es_compositor", text="UV Editor")
+        box.prop(self, "es_texture_node", text="Compositor")
+        box.prop(self, "es_geometry_node", text="Texture Node Editor")
+        box.prop(self, "es_shader_editor", text="Geometry Node Editor")
+        box.prop(self, "es_sequence_editor", text="Shader Editor")
+        box.prop(self, "es_clip_editor", text="Movie Clip Editor")
+        box.prop(self, "es_dopesheet", text="Dope Sheet")
+        box.prop(self, "es_timeline", text="Timeline")
+        box.prop(self, "es_fcurves", text="Graph Editor")
+        box.prop(self, "es_drivers", text="Drivers")
+        box.prop(self, "es_nla_editor", text="Nonlinear Animation")
+        box.prop(self, "es_text_editor", text="Text Editor")
+        box.prop(self, "es_console", text="Python Console")
+        box.prop(self, "es_info", text="Info")
+        box.prop(self, "es_outliner", text="Outliner")
+        box.prop(self, "es_properties", text="Properties")
+        box.prop(self, "es_files", text="File Browser")
+        box.prop(self, "es_assets", text="Asset Browser")
+        box.prop(self, "es_preferences", text="Preferences")
         box.separator()
 
 ####################################
